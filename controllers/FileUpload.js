87754ff42df6888eef3cc25637be35afb871c8cc
Rex Egonio Jr. <rex.egonio@cepatkredit.com>
2024-10-11 09:04:21 +0800

    Fix file upload
    - to upload files converted to BLOB

--- controllers/FileUpload.js
+++ controllers/FileUpload.js
@@ -1,4 +1,22 @@
 import axios from "axios";
+import fs from 'fs'
+
+async function ConvertToBlob(files) {
+    const promises = files.map(file => {
+        return new Promise((resolve, reject) => {
+            fs.readFile(file.path, (err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve({ file, data, });
+                }
+            });
+        });
+    });
+
+    const fileData = await Promise.all(promises);
+    return fileData;
+}
 
 export async function GetFileType(req, res) {
     const { API } = process.env;
@@ -25,7 +43,7 @@ export async function GetFileType(req, res) {
 
 export async function GetFileList(req, res) {
     const { API } = process.env;
-    await axios.get(`${API}/getFileList/${req.params.LAI}/${req.params.PRID}`)
+    await axios.get(`${API}/getFileList/${req.params.LAI}/${req.params.PRID}/${req.params.USRID}`)
         .then((result) => {
             res.json(result.data)
         })
@@ -49,7 +67,22 @@ export async function GetFileList(req, res) {
 export async function UploadFileReq(req, res) {
     const { API } = process.env;
 
-    await axios.post(`${API}/uploadFileReq`, req.body, {
+    const formData = new FormData();
+    const FileContainer = await ConvertToBlob(req.files);
+    formData.append('client', req.body.client)
+    formData.append('docsID_list', req.body.docsID_list)
+    formData.append('status_list', req.body.status_list)
+    formData.append('remarks_list', req.body.remarks_list)
+    formData.append('docStatus_list', req.body.docStatus_list)
+    formData.append('prid', req.body.prid)
+    formData.append('Uploader', req.body.Uploader)
+
+    FileContainer.map(async (x) => {
+        const FileHolder = new File([x.data], { type: x.file.mimetype });
+        formData.append('files', FileHolder, x.file.originalname)
+    })
+
+    await axios.post(`${API}/uploadFileReq`, formData, {
         headers: {
             'Content-Type': 'multipart/form-data'
         }
@@ -58,6 +91,7 @@ export async function UploadFileReq(req, res) {
             res.json(result.data)
         })
         .catch(error => {
+            console.log(error)
             console.log('                                         ');
             console.log('=========================================');
             console.log('[POST REQUEST]');
@@ -71,13 +105,28 @@ export async function UploadFileReq(req, res) {
                 Message: error.message,
                 Status: error.status
             })
-        });
+        })
 }
 
 export async function UploadFileFin(req, res) {
     const { API } = process.env;
 
-    await axios.post(`${API}/uploadFileFin`, req.body, {
+    const formData = new FormData();
+    const FileContainer = await ConvertToBlob(req.files);
+    formData.append('client', req.body.client)
+    formData.append('docsID_list', req.body.docsID_list)
+    formData.append('dfn_list', req.body.dfn_list)
+    formData.append('remarks_list', req.body.remarks_list)
+    formData.append('docStatus_list', req.body.docStatus_list)
+    formData.append('prid', 'IC')
+    formData.append('Uploader', req.body.Uploader)
+
+    FileContainer.map(async (x) => {
+        const FileHolder = new File([x.data], { type: x.file.mimetype });
+        formData.append('files', FileHolder, x.file.originalname)
+    })
+
+    await axios.post(`${API}/uploadFileFin`, formData, {
         headers: {
             'Content-Type': 'multipart/form-data'
         }
