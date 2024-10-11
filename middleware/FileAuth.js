0ede3a91fd537672658eac720c59b31539fa9209
Rex Egonio Jr. <rex.egonio@cepatkredit.com>
2024-10-11 09:04:21 +0800

    Fix file upload
    - to upload files converted to BLOB

---
+++ middleware/FileAuth.js
@@ -0,0 +1,11 @@
+import multer from 'multer'
+const storage = multer.diskStorage({
+
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname)
+    }
+})
+
+export const upload = multer({ storage })
+
+
